f299e77c5b99fa115b330991096e92997eb483dd
HidayaddinPashayev <hpashayev18207@ada.edu.az>
2026-04-02 16:57:12 +0400

    Add Services

--- src/main/java/az/edu/ada/wm2/lab6/repository/ProductRepository.java
+++ src/main/java/az/edu/ada/wm2/lab6/repository/ProductRepository.java
@@ -1,7 +1,14 @@
 package az.edu.ada.wm2.lab6.repository;
 
 import az.edu.ada.wm2.lab6.model.Product;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, UUID> {}
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    List<Product> findByExpirationDateBefore(LocalDate date);
+
+    List<Product> findByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
+}

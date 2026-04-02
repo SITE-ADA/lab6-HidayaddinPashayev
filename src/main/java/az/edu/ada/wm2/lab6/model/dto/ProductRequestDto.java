1ffe541eacf7584c8d2d2d5b18944fca6f47013d
HidayaddinPashayev <hpashayev18207@ada.edu.az>
2026-04-02 17:32:03 +0400

    add DTOs

--- src/main/java/az/edu/ada/wm2/lab6/model/dto/ProductRequestDto.java
+++ src/main/java/az/edu/ada/wm2/lab6/model/dto/ProductRequestDto.java
@@ -4,52 +4,20 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductRequestDto {
     private String productName;
     private BigDecimal price;
     private LocalDate expirationDate;
     private List<UUID> categoryIds;
-
-    public ProductRequestDto() {
-    }
-
-    public ProductRequestDto(String productName, BigDecimal price, LocalDate expirationDate, List<UUID> categoryIds) {
-        this.productName = productName;
-        this.price = price;
-        this.expirationDate = expirationDate;
-        this.categoryIds = categoryIds;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public LocalDate getExpirationDate() {
-        return expirationDate;
-    }
-
-    public void setExpirationDate(LocalDate expirationDate) {
-        this.expirationDate = expirationDate;
-    }
-
-    public List<UUID> getCategoryIds() {
-        return categoryIds;
-    }
-
-    public void setCategoryIds(List<UUID> categoryIds) {
-        this.categoryIds = categoryIds;
-    }
 }
--- src/main/java/az/edu/ada/wm2/lab6/model/dto/ProductResponseDto.java
+++ src/main/java/az/edu/ada/wm2/lab6/model/dto/ProductResponseDto.java
@@ -4,62 +4,21 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductResponseDto {
     private UUID id;
     private String productName;
     private BigDecimal price;
     private LocalDate expirationDate;
     private List<String> categoryNames;
-
-    public ProductResponseDto() {
-    }
-
-    public ProductResponseDto(UUID id, String productName, BigDecimal price, LocalDate expirationDate, List<String> categoryNames) {
-        this.id = id;
-        this.productName = productName;
-        this.price = price;
-        this.expirationDate = expirationDate;
-        this.categoryNames = categoryNames;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public LocalDate getExpirationDate() {
-        return expirationDate;
-    }
-
-    public void setExpirationDate(LocalDate expirationDate) {
-        this.expirationDate = expirationDate;
-    }
-
-    public List<String> getCategoryNames() {
-        return categoryNames;
-    }
-
-    public void setCategoryNames(List<String> categoryNames) {
-        this.categoryNames = categoryNames;
-    }
 }
